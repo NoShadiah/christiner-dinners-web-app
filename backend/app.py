bce40d803d81df6dd54fe8415904cf0b03d05d22
NoShadiah <nabawangashadiah20@gmail.com>
2023-03-28 14:36:40 +0300

    Blue prints

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,8 @@ from models.users.model import User
 from models.food_categories.model import FoodCategory 
 # from models.settings.model import Settings
 # from models.menu.model import MenuItem
-# from models.gallery.model import Gallery
-# from models.food_items.model import FoodItem
+from models.gallery.model import Gallery
+from models.food_items.model import FoodItem
 # from models.orders.model import Order
 from flask_jwt_extended import JWTManager
 
