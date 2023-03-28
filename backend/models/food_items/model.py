0d26d3b8a398518387ac06f8a7f608e296b9b59a
NoShadiah <nabawangashadiah20@gmail.com>
2023-03-28 13:46:38 +0300

    Food itms model

---
+++ backend/models/food_items/model.py
@@ -0,0 +1,25 @@
+from models.db import db
+from datetime import datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class FoodCategory(db.Model):
+    id:int
+    name:str
+    image:str
+    description:str
+    registered_at:str
+    
+
+    __tablename__ = "Food_Categories"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    image = db.Column(db.String(200))
+    description = db.Column(db.String(250))
+    registered_by = db.Column(db.Integer, db.ForeignKey('users.id'))
+    registered_at = db.Column(db.String(250), default=datetime.now())
+    updated_at = db.Column(db.String(250), onupdate=datetime.now())
+
+    def __repr__():
+        return(f"<Category>..............{FoodCategory.name}")
